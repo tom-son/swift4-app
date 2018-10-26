53dfb6da0c3f46064837bbcb3cbcadb4260bea4b
Thompson Doan <thompson.doan@gmail.com>
2018-10-26 15:04:14 +1100

    Initial Commit

---
+++ StudentManagementApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentManagementApp
+//
+//  Created by Test on 26/10/18.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
